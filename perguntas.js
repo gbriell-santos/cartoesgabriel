c165ac211c554096c08b3fa1cc2046a0084e663d
gbriell-santos <gabriel.gomes25@escola.pr.gov.br>
2025-06-30 10:37:02 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao (
+'Programação',
+'O que é Python', 
+'É uma linguagem de programação.');
+
+criaCartao (
+'Geografia',
+'Qual a capital da França? ', 
+'A capital da França é Paris');
+
+criaCartao (
+'Programação',
+'O que é uma função?', 
+'Uma função é um bloco de código que executa uma tarefa.');
+
+criaCartao (
+'Idioma',
+'Como se diz "oi" em inglês?',
+' Oi em inglês é Hi');
+
+criaCartao (
+  'Matemática',
+  'Quanto é 5 + 3?',
+  '5 + 3 é igual a 8');
+
+criaCartao('Biologia',
+'Qual órgão é responsável por bombear o sangue?', 
+'O coração é responsável por bombear o sangue');
